706b3e4ce74e06cc1e8cd49039e2a116899a7a0e
Nikolai Ivanov <nikolai.ivanov.2038@gmail.com>
2026-04-03 10:51:29 +0300

    Install via copy and not symlink

--- install.sh
+++ install.sh
@@ -3,10 +3,15 @@
 main() {
   if [ ! -f ./hyprland-screenshoter.sh ]; then
     echo "hyprland-screenshoter.sh not found in the current directory."
-    exit 1
+    return 1
   fi
 
-  sudo ln -sf "${PWD}/hyprland-screenshoter.sh" /usr/local/bin/hyprland-screenshoter || return "$?"
+  # NOTE: Previously I used symlink, but don't want to be dependent on it anymore
+  if [ -e /usr/local/bin/hyprland-screenshoter ]; then
+    sudo rm /usr/local/bin/hyprland-screenshoter || return "$?"
+  fi
+
+  sudo cp -T "${PWD}/hyprland-screenshoter.sh" /usr/local/bin/hyprland-screenshoter || return "$?"
 }
 
 main "$@" || exit "$?"
